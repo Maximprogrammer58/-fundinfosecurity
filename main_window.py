ae21cd10ca8d6585cacf3daa97f71bc67b7d1099
Maximprogrammer58 <maks.shamen2004@mail.ru>
2024-02-11 23:29:52 +0400

    A dextope application for the Caesar cipher has been written in pyqt5

---
+++ main_window.py
@@ -0,0 +1,98 @@
+import sys
+import crypto
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowIcon(QtGui.QIcon("D:\icon.jpg"))
+        MainWindow.setFixedSize(820, 687)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 20, 171, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(50, 90, 191, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(50, 300, 113, 22))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(50, 270, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(50, 340, 181, 61))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.caesar_cipher)
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(50, 430, 191, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setGeometry(QtCore.QRect(50, 120, 651, 131))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit_2.setGeometry(QtCore.QRect(50, 460, 651, 131))
+        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 820, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Шифр Цезаря"))
+        self.label_2.setText(_translate("MainWindow", "Текст для шифрования"))
+        self.label_3.setText(_translate("MainWindow", "Ключ"))
+        self.pushButton.setText(_translate("MainWindow", "Зашифровать"))
+        self.label_4.setText(_translate("MainWindow", "Зашифрованный текст"))
+
+    def __warning_icon(self, text, info):
+        error = QtWidgets.QMessageBox()
+        error.setIcon(QMessageBox.Warning)
+        error.setWindowTitle(text)
+        error.setText(info)
+        error.exec_()
+
+    def caesar_cipher(self):
+        text = self.plainTextEdit.toPlainText()
+        key = self.lineEdit_2.text()
+        if (key.isdigit() and int(key) > 0):
+            encrypted_text = crypto.caesar_cipher(text, int(key))
+            self.plainTextEdit_2.setPlainText(encrypted_text)
+        else:
+            self.__warning_icon("Предупреждение", "Введите корректное значение ключа key")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
