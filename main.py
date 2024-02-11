a95ac29f6fc41a7b2f63029ad454d0dee8fce049
Maximprogrammer58 <maks.shamen2004@mail.ru>
2024-02-11 17:32:22 +0400

    The text has been decrypted and the key has been written to a jsin file

--- main.py
+++ main.py
@@ -1,17 +1,60 @@
 import crypto
+import file_handler
 
 def main():
+    '''
+    # part 1
     alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-    text = '''Использование автоматизированных систем во всех сферах деятельности человека, основанных на применении современных информационно-коммуникационных технологий, выдвинуло целый ряд проблем перед разработчиками и пользователями этих систем. Одна из наиболее острых проблем – проблема информационной безопасности, которую необходимо обеспечивать, контролировать, а также создавать условия для ее управления.'''
-    key = crypto.create_key()
-    print(alphabet)
-    print(text)
-    print(crypto.caesar_cipher_encode(text.upper(), 3))
-    print(key)
-    print(crypto.encryption(text.upper(), key))
+    text = file_handler.read_file("texts/part1/text.txt")
+    key = file_handler.read_file("texts/part1/key.txt")
+    encrypted_text = crypto.caesar_cipher(text, int(key))
+    file_handler.write_file("texts/part1/encrypted_text.txt", encrypted_text)
+    '''
+
+    #part 2
+    code = file_handler.read_file("texts/part2/code.txt")
+
+    print(crypto.write_frequency("freq.json"))
+    print(crypto.frequency_analysis(code))
+
+    code = code.replace("М", " ")
+    code = code.replace("Ф", "М")
+    code = code.replace("И", "Ф")
+    code = code.replace(">", "И")
+    code = code.replace("Е", "С")
+    code = code.replace("О", "Е")
+    code = code.replace("Ь", "Щ")
+    code = code.replace("А", "Ь")
+    code = code.replace("Л", "Я")
+    code = code.replace("Р", "З")
+    code = code.replace("Д", "Р")
+    code = code.replace("1", "О")
+    code = code.replace("r", "Т")
+    code = code.replace("Х", "Н")
+    code = code.replace("4", "А")
+    code = code.replace("c", "Д")
+    code = code.replace("П", "Ж")
+    code = code.replace("2", "П")
+    code = code.replace("К", "Ю")
+    code = code.replace("8", "К")
+    code = code.replace("Й", "Х")
+    code = code.replace("7", "Й")
+    code = code.replace("Ч", "Ц")
+    code = code.replace("<", "Ч")
+    code = code.replace("a", "В")
+    code = code.replace("b", "Г")
+    code = code.replace("Ы", "Ш")
+    code = code.replace("\n", "Ы")
+    code = code.replace("Б", "Э")
+    code = code.replace("5", "Б")
+    code = code.replace("У", "Л")
+    code = code.replace("t", "У")
+
+    print(code)
 
 
 if __name__ == "__main__":
     main()
-    d = crypto.write_frequency("freq.json")
-    sorted_tuple = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+
+
+
